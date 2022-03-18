c2e68bfc3aec5a9067b33c0a8ec053feb02f40d3
CarolynnFleming <cfcatpink@gmail.com>
2022-03-18 16:07:47 -0700

    food test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Anime = require('../lib/models/Anime');
 const Book = require('../lib/models/Book');
+const Food = require('../lib/models/Food');
 
 describe('api-routes', () => {
   beforeEach(() => {
@@ -140,62 +141,62 @@ describe('api-routes', () => {
 
 
 
-  
-  it('should be able to create an books', async () => {
+
+  it('should be able to create an food', async () => {
     const res = await request(app)
-    .post('/api/v1/books')
-    .send({ title: 'FairyTale', author:'Natsu' });
+    .post('/api/v1/foods')
+    .send({ dish_name: 'FairyTale', dish_type:'Natsu' });
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      title: 'FairyTale',
-      author: 'Natsu'
+      dish_name: 'FairyTale',
+      dish_type: 'Natsu'
     });
   });
 
   it('should be able to list an book by id', async () => {
-    const book = await Book.insert({ title: 'FairyTale', author: 'Natsu' });
-    const res = await request(app).get(`/api/v1/books/${book.id}`);
+    const food = await Food.insert({ dish_name: 'FairyTale', dish_type: 'Natsu' });
+    const res = await request(app).get(`/api/v1/foods/${food.id}`);
 
-    expect(res.body).toEqual(book);
+    expect(res.body).toEqual(food);
   })
-  it('should be able to list books', async () => {
-    await Book.insert({ title: 'FairyTale', author: 'Natsu' });
-    const res = await request(app).get('/api/v1/books');
+  it('should be able to list foods', async () => {
+    await Food.insert({ dish_name: 'FairyTale', dish_type: 'Natsu' });
+    const res = await request(app).get('/api/v1/foods');
 
     expect(res.body).toEqual([
       {
         id: expect.any(String),
-        title: 'FairyTale',
-        author: 'Natsu'
+        dish_name: 'FairyTale',
+        dish_type: 'Natsu'
       }
     ]);
   });
 
-  it('should be able to update books', async () => {
-    const book = await Book.insert({ title: 'FairyTale', author: 'Natsu' }); 
+  it('should be able to update foods', async () => {
+    const food = await Food.insert({ dish_name: 'FairyTale', dish_type: 'Natsu' }); 
     const res = await request(app)
-    .patch(`/api/v1/books/${book.id}`)
-    .send({title: 'Naruto', author: 'Sakura' });
+    .patch(`/api/v1/foods/${food.id}`)
+    .send({ dish_name: 'Naruto', dish_type: 'Sakura' });
 
     const expected = {
       id: expect.any(String),
-      title: 'Naruto',
-      author: 'Sakura',
+      dish_name: 'Naruto',
+      dish_type: 'Sakura',
   
     };
 
     expect(res.body).toEqual(expected);
-    expect(await Book.getById(book.id)).toEqual(expected);
+    expect(await Food.getById(food.id)).toEqual(expected);
   });
 
-  it('should be able to delete an book', async () => {
-    const book = await Book.insert({ title: 'FairyTale', author: 'Natsu' });
+  it('should be able to delete an food', async () => {
+    const food = await Food.insert({ dish_name: 'FairyTale', dish_type: 'Natsu' });
 
-  const res = await request(app).delete(`/api/v1/books/${book.id}`);
+  const res = await request(app).delete(`/api/v1/foods/${food.id}`);
 
-  expect(res.body).toEqual(book);
-  expect(await Book.getById(book.id)).toBeNull();
+  expect(res.body).toEqual(food);
+  expect(await Food.getById(food.id)).toBeNull();
   });
 });
 
